ff373bd76261eafc8730142f8aabea2b775bb586
pala0118 <pavellobanok@gmail.com>
2018-11-04 22:11:37 +0300

    added kotlin module for future

---
+++ aircompany-rest-kotlin/src/main/kotlin/com/qwerfghi/aircompany/Application.kt
@@ -0,0 +1,25 @@
+package com.qwerfghi.aircompany
+
+import org.springframework.boot.SpringApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+class Application {
+
+    fun main(args: Array<String>) {
+        SpringApplication.run(Application::class, *args)
+    }
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry?) {
+                registry?.addMapping("/**")
+                        ?.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                        ?.allowedOrigins("http://localhost:4200")
+            }
+        }
+    }
+}
