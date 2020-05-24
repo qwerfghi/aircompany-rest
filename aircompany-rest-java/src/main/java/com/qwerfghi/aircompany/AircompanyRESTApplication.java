fbe30f2b1ef3b4c52b9ecb027d8a25bfa357a23d
Pavel Lobanok <pavellobanok@gmail.com>
2020-05-25 02:01:49 +0300

    added lombok, rewrite properties file into yaml, started to remove unnecessary DTOs

--- aircompany-rest-java/src/main/java/com/qwerfghi/aircompany/AircompanyRESTApplication.java
+++ aircompany-rest-java/src/main/java/com/qwerfghi/aircompany/AircompanyRESTApplication.java
@@ -3,9 +3,14 @@ package com.qwerfghi.aircompany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class AircompanyRESTApplication {
@@ -24,5 +29,16 @@ public class AircompanyRESTApplication {
                         .allowedOrigins("http://localhost:4200", "http://localhost:5200");
             }
         };
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.qwerfghi.aircompany.entity");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        return em;
     }
 }
