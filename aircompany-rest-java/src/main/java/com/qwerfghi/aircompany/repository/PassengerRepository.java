20dd17d2c4c79e1bb073c7265a5f115b00b235bd
Pavel Lobanok <pavellobanok@gmail.com>
2020-11-20 01:16:37 +0300

    Bring back file, that was deleted during merge

---
+++ aircompany-rest-java/src/main/java/com/qwerfghi/aircompany/repository/PassengerRepository.java
@@ -0,0 +1,9 @@
+package com.qwerfghi.aircompany.repository;
+
+import com.qwerfghi.aircompany.entity.model.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerRepository extends JpaRepository<Passenger, Integer> {
+}
